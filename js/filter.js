5c88c5d9ea2cd18b6407d0160f80d38227395c2a
nkaira <nkaira89@mail.ru>
2022-05-31 18:34:28 +0300

    add new project

---
+++ js/filter.js
@@ -0,0 +1,21 @@
+const Filter = (function () {
+  const Filter = function () {
+    this.value = 'all';
+    this.buttonFilterAll = document.getElementById('taskButtonFilterAll');
+    this.buttonFilterComplete = document.getElementById('taskButtonFilterComplete');
+    this.buttonFilterActive = document.getElementById('taskButtonFilterActive');
+  };
+
+  Filter.prototype = {
+    setButtonEvent: function (button, eventName, func, value) {
+      button.addEventListener(eventName, function () {
+        return func(value);
+      });
+    },
+
+    getFilterName: function () {
+      return this.value;
+    },
+  }
+  return Filter;
+})()
